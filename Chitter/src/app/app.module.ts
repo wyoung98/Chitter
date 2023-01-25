2fe9d2d59923f1012827500123a392dfc2d21d98
wesyoung98 <107873633+wyoung98@users.noreply.github.com>
2023-01-25 14:59:40 -0500

    Adding other components

--- Chitter/src/app/app.module.ts
+++ Chitter/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BudgetsComponent } from './budgets/budgets.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GoalsComponent } from './goals/goals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BudgetsComponent,
+    AccountsComponent,
+    ProfileComponent,
+    GoalsComponent
   ],
   imports: [
     BrowserModule,
